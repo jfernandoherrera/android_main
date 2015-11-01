74bfca576a927110f29ec6b7d52ec0e3ca0a6370
jfernandoherrera <jfernandoherrera1@gmail.com>
2015-11-01 05:03:31 -0500

    Venue: setupDay

--- app/src/main/java/com/amtechventures/tucita/activities/venue/VenueActivity.java
+++ app/src/main/java/com/amtechventures/tucita/activities/venue/VenueActivity.java
@@ -2,9 +2,10 @@ package com.amtechventures.tucita.activities.venue;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-
+import android.widget.RadioButton;
 import com.amtechventures.tucita.R;
+import java.util.Calendar;
+
 
 public class VenueActivity extends AppCompatActivity {
 
@@ -15,7 +16,65 @@ public class VenueActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_venue);
 
+        setup();
     }
 
+    private void setup(){
 
+    setupDay();
+    }
+
+    private void setupDay(){
+
+        Calendar c = Calendar.getInstance();
+
+        int day = c.get(Calendar.DAY_OF_WEEK);
+
+        RadioButton radioButton;
+
+        switch (day){
+
+            case Calendar.MONDAY:
+
+                radioButton = (RadioButton) findViewById(R.id.radioButton1);
+
+                break;
+            case Calendar.TUESDAY:
+
+                radioButton = (RadioButton) findViewById(R.id.radioButton2);
+
+                break;
+            case Calendar.WEDNESDAY:
+
+                radioButton = (RadioButton) findViewById(R.id.radioButton3);
+
+                break;
+            case Calendar.THURSDAY:
+
+                radioButton = (RadioButton) findViewById(R.id.radioButton4);
+
+                break;
+            case Calendar.FRIDAY:
+
+                radioButton = (RadioButton) findViewById(R.id.radioButton5);
+
+                break;
+            case Calendar.SATURDAY:
+
+                radioButton = (RadioButton) findViewById(R.id.radioButton6);
+
+                break;
+            case Calendar.SUNDAY:
+
+                radioButton = (RadioButton) findViewById(R.id.radioButton7);
+
+                break;
+            default:
+
+                radioButton = (RadioButton) findViewById(R.id.radioButton1);
+
+                break;
+        }
+        radioButton.toggle();
+    }
 }
