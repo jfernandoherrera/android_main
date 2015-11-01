423a3e47c7ff03bba38a6107bf2e5a26d263b42a
jfernandoherrera <jfernandoherrera1@gmail.com>
2015-11-01 04:24:45 -0500

    Venue: Refactoring

--- app/src/main/java/com/amtechventures/tucita/activities/venue/VenueActivity.java
+++ app/src/main/java/com/amtechventures/tucita/activities/venue/VenueActivity.java
@@ -1,23 +1,12 @@
 package com.amtechventures.tucita.activities.venue;
 
-import android.net.Uri;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+
 
 import com.amtechventures.tucita.R;
 
-public class VenueActivity extends AppCompatActivity implements ReviewFragment.OnFragmentInteractionListener,
-
-        TopServicesFragment.OnFragmentInteractionListener,
-
-        AboutFragment.OnFragmentInteractionListener,
-
-        FullMenuFragment.OnFragmentInteractionListener,
-
-        OpeningHoursFragment.OnFragmentInteractionListener{
+public class VenueActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +18,4 @@ public class VenueActivity extends AppCompatActivity implements ReviewFragment.O
     }
 
 
-    @Override
-    public void onFragmentInteraction(Uri uri) {
-
-    }
 }
