a064ff9bdae80c0df1ce8e100148502a82f3a89c
Andres <andres@andrescastano.com>
2015-11-04 22:43:37 +0000

    develop - adding fabric

--- app/src/main/java/com/amtechventures/tucita/app/Application.java
+++ app/src/main/java/com/amtechventures/tucita/app/Application.java
@@ -1,10 +1,11 @@
 package com.amtechventures.tucita.app;
 
 import com.parse.Parse;
-
 import com.parse.ParseObject;
 import com.facebook.FacebookSdk;
 import com.parse.ParseFacebookUtils;
+import io.fabric.sdk.android.Fabric;
+import com.crashlytics.android.Crashlytics;
 import com.amtechventures.tucita.utils.strings.Strings;
 import com.amtechventures.tucita.model.domain.category.Category;
 
@@ -13,6 +14,8 @@ public class Application extends android.app.Application {
     public void onCreate() {
 
         super.onCreate();
+        
+        Fabric.with(this, new Crashlytics());
 
         String app_parse_id = Strings.APP_PARSE_ID;
 
