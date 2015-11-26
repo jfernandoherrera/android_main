1673dcff2235edbaa2131fc9d1dabc0cbbb491ab
jfernandoherrera <jfernandoherrera1@gmail.com>
2015-11-26 00:49:22 -0500

    Venue_feature: no results and setup of UI to subcategorizedvenuesforcity feature

--- app/src/main/java/com/amtechventures/tucita/activities/search/advanced/AdvancedSearchActivity.java
+++ app/src/main/java/com/amtechventures/tucita/activities/search/advanced/AdvancedSearchActivity.java
@@ -1,51 +1,23 @@
 package com.amtechventures.tucita.activities.search.advanced;
 
-import android.app.ProgressDialog;
-import android.location.Location;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.ProgressBar;
 import com.amtechventures.tucita.R;
-import com.amtechventures.tucita.activities.search.adapters.AdvancedSearchAdapter;
-import com.amtechventures.tucita.model.context.location.LocationContext;
-import com.amtechventures.tucita.model.context.service.ServiceCompletion;
-import com.amtechventures.tucita.model.context.service.ServiceContext;
-import com.amtechventures.tucita.model.context.subcategory.SubCategoryContext;
-import com.amtechventures.tucita.model.context.venue.VenueCompletion;
-import com.amtechventures.tucita.model.context.venue.VenueContext;
 import com.amtechventures.tucita.model.domain.category.CategoryAttributes;
-import com.amtechventures.tucita.model.domain.service.Service;
-import com.amtechventures.tucita.model.domain.subcategory.SubCategory;
-import com.amtechventures.tucita.model.domain.venue.Venue;
-import com.amtechventures.tucita.model.error.AppError;
-import com.amtechventures.tucita.utils.views.AlertDialogError;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import java.util.ArrayList;
-import java.util.List;
 
-public class AdvancedSearchActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks{
 
-    private ProgressDialog progress;
-    private RecyclerView.LayoutManager layoutManager;
-    private RecyclerView recyclerView;
-    private RecyclerView.Adapter adapter;
-    private List<Venue> venues = new ArrayList<>();
-    private VenueContext venueContext;
-    private ServiceContext serviceContext;
-    private SubCategoryContext subCategoryContext;
+public class AdvancedSearchActivity extends AppCompatActivity {
+
+
     private Toolbar toolbar;
     private String name;
-    private SubCategory subCategory;
-    private ArrayList priceStrings = new ArrayList<>();
-    private LocationContext locationContext;
+    private VenuesResultFragment venuesResultFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,35 +26,39 @@ public class AdvancedSearchActivity extends AppCompatActivity implements GoogleA
 
         setContentView(R.layout.activity_advanced_search);
 
-        setupProgress();
-
-        venueContext = VenueContext.context(venueContext);
-
-        subCategoryContext = SubCategoryContext.context(subCategoryContext);
-
-        serviceContext = ServiceContext.context(serviceContext);
-
-        locationContext = LocationContext.context(locationContext, this, this, this);
-
         name = getIntent().getStringExtra(CategoryAttributes.name);
 
-        recyclerView = (RecyclerView)findViewById(R.id.recycler_view);
+        venuesResultFragment = new VenuesResultFragment();
 
-        recyclerView.setHasFixedSize(true);
+        setVenuesResultFragment();
 
-        layoutManager = new GridLayoutManager(getApplicationContext(), 1);
-
-        recyclerView.setLayoutManager(layoutManager);
+        showVenuesResultFragment();
 
         setToolbar();
     }
 
-    private void setupProgress(){
+    private void setVenuesResultFragment(){
 
-        progress = ProgressDialog.show(this, getResources().getString(R.string.dialog_progress_title),
+        FragmentManager fragmentManager = getSupportFragmentManager();
 
-                getResources().getString(R.string.dialog_advanced_search_progress_message), true);
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+        transaction.add(R.id.myDrawView1, venuesResultFragment);
+
+        transaction.commit();
     }
+
+    private void showVenuesResultFragment(){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+        transaction.show(venuesResultFragment);
+
+        transaction.commit();
+    }
+
 
     private void setToolbar(){
 
@@ -92,87 +68,6 @@ public class AdvancedSearchActivity extends AppCompatActivity implements GoogleA
 
             setSupportActionBar(toolbar);
         }
-    }
-
-    @Override
-    protected void onPause() {
-
-        super.onPause();
-
-        subCategoryContext.cancelQuery();
-
-        serviceContext.cancelQuery();
-
-        venueContext.cancelQuery();
-    }
-
-    private void setupPriceFrom(){
-
-            for (Venue venue : venues) {
-
-                int price = serviceContext.getPricesFrom(subCategory, venue);
-
-                if(price != 0) {
-
-                    priceStrings.add(String.valueOf(price));
-                }
-            }
-    }
-
-    private void setupVenues(List<Service> services) {
-
-        Location lastLocation = locationContext.getLastLocation();
-
-        if (lastLocation != null) {
-
-             venueContext.loadSubCategorizedNearVenues(services, lastLocation, new VenueCompletion.ErrorCompletion() {
-
-                @Override
-                public void completion(List<Venue> venueList, AppError error) {
-
-                    if (venueList != null) {
-
-                        venues.clear();
-
-                        venues.addAll(venueList);
-
-                        setupPriceFrom();
-
-                        adapter = new AdvancedSearchAdapter(venues, priceStrings, subCategory.getName());
-
-                        recyclerView.setAdapter(adapter);
-
-                        progress.dismiss();
-                    }
-                }
-
-            });
-
-        }
-    }
-
-    private void setupGrid() {
-
-                subCategory = subCategoryContext.findSubCategory(name);
-
-                serviceContext.loadSubCategorizedServices(subCategory, new ServiceCompletion.ErrorCompletion() {
-                    @Override
-                    public void completion(List<Service> servicesList, AppError error) {
-
-                        if (servicesList != null) {
-
-                            setupVenues(servicesList);
-
-                        }else{
-
-                            progress.dismiss();
-
-                            AlertDialogError alertDialogError = new AlertDialogError();
-
-                            alertDialogError.noInternetConnectionAlert(getApplicationContext());
-                        }
-                    }
-                });
     }
 
     @Override
@@ -191,23 +86,5 @@ public class AdvancedSearchActivity extends AppCompatActivity implements GoogleA
         finish();
 
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onConnected(Bundle bundle) {
-
-    locationContext.setLocation();
-
-            setupGrid();
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-
     }
 }
---
+++ app/src/main/java/com/amtechventures/tucita/activities/search/advanced/VenuesResultFragment.java
@@ -0,0 +1,202 @@
+package com.amtechventures.tucita.activities.search.advanced;
+
+
+import android.app.ProgressDialog;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.amtechventures.tucita.R;
+import com.amtechventures.tucita.activities.search.adapters.AdvancedSearchAdapter;
+import com.amtechventures.tucita.model.context.location.LocationContext;
+import com.amtechventures.tucita.model.context.service.ServiceCompletion;
+import com.amtechventures.tucita.model.context.service.ServiceContext;
+import com.amtechventures.tucita.model.context.subcategory.SubCategoryContext;
+import com.amtechventures.tucita.model.context.venue.VenueCompletion;
+import com.amtechventures.tucita.model.context.venue.VenueContext;
+import com.amtechventures.tucita.model.domain.category.CategoryAttributes;
+import com.amtechventures.tucita.model.domain.service.Service;
+import com.amtechventures.tucita.model.domain.subcategory.SubCategory;
+import com.amtechventures.tucita.model.domain.venue.Venue;
+import com.amtechventures.tucita.model.error.AppError;
+import com.amtechventures.tucita.utils.views.AlertDialogError;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VenuesResultFragment extends Fragment implements GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks {
+
+    private ProgressDialog progress;
+    private RecyclerView.LayoutManager layoutManager;
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private List<Venue> venues = new ArrayList<>();
+    private VenueContext venueContext;
+    private ServiceContext serviceContext;
+    private SubCategoryContext subCategoryContext;
+    private String name;
+    private SubCategory subCategory;
+    private ArrayList priceStrings = new ArrayList<>();
+    private LocationContext locationContext;
+    private TextView noResults;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_venues_result, container, false);
+
+        setupProgress();
+
+        venueContext = VenueContext.context(venueContext);
+
+        subCategoryContext = SubCategoryContext.context(subCategoryContext);
+
+        serviceContext = ServiceContext.context(serviceContext);
+
+        locationContext = LocationContext.context(locationContext, getContext(), this, this);
+
+        name = getActivity().getIntent().getStringExtra(CategoryAttributes.name);
+
+        recyclerView = (RecyclerView)rootView.findViewById(R.id.recycler_view);
+
+        recyclerView.setHasFixedSize(true);
+
+        noResults = (TextView) rootView.findViewById(R.id.noResults);
+
+        noResults.setVisibility(View.GONE);
+
+        layoutManager = new GridLayoutManager(getContext(), 1);
+
+        recyclerView.setLayoutManager(layoutManager);
+
+        return rootView;
+    }
+
+    private void setupProgress(){
+
+        progress = ProgressDialog.show(getContext(), getResources().getString(R.string.dialog_progress_title),
+
+                getResources().getString(R.string.dialog_advanced_search_progress_message), true);
+    }
+
+
+    @Override
+    public void onPause() {
+
+        super.onPause();
+
+        subCategoryContext.cancelQuery();
+
+        serviceContext.cancelQuery();
+
+        venueContext.cancelQuery();
+    }
+
+
+    private void setupPriceFrom(){
+
+        for (Venue venue : venues) {
+
+            int price = serviceContext.getPricesFrom(subCategory, venue);
+
+            if(price != 0) {
+
+                priceStrings.add(String.valueOf(price));
+            }
+        }
+    }
+
+    private void setupVenues(List<Service> services) {
+
+        Location lastLocation = locationContext.getLastLocation();
+
+        if (lastLocation != null) {
+
+            venueContext.loadSubCategorizedNearVenues(services, lastLocation, new VenueCompletion.ErrorCompletion() {
+
+                @Override
+                public void completion(List<Venue> venueList, AppError error) {
+
+                    if (venueList != null) {
+
+                        if(venueList.isEmpty()){
+                            
+                            noResults.setVisibility(View.VISIBLE);
+                        }
+
+                        venues.clear();
+
+                        venues.addAll(venueList);
+
+                        setupPriceFrom();
+
+                        adapter = new AdvancedSearchAdapter(venues, priceStrings, subCategory.getName());
+
+                        recyclerView.setAdapter(adapter);
+
+                        progress.dismiss();
+                    }
+                }
+
+            });
+
+        }
+    }
+
+    private void setupGrid() {
+
+        subCategory = subCategoryContext.findSubCategory(name);
+
+        serviceContext.loadSubCategorizedServices(subCategory, new ServiceCompletion.ErrorCompletion() {
+            @Override
+            public void completion(List<Service> servicesList, AppError error) {
+
+                if (servicesList != null) {
+
+                    setupVenues(servicesList);
+
+                } else {
+
+                    progress.dismiss();
+
+                    AlertDialogError alertDialogError = new AlertDialogError();
+
+                    alertDialogError.noInternetConnectionAlert(getContext());
+                }
+            }
+        });
+    }
+
+
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+        locationContext.setLocation();
+
+        setupGrid();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+}
