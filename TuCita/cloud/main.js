e795d49d49e8f5dac90c27ad30d13af6dd9e1172
jose fernando herrera <jose@amtechventures.com>
2016-01-15 10:39:36 -0500

    cloud code review refactoring

--- TuCita/cloud/main.js
+++ TuCita/cloud/main.js
@@ -1,8 +1,3 @@
-
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-
-
 Parse.Cloud.afterSave("Review", function(request) {
 
 var venue = request.object.get("venue");
@@ -13,36 +8,40 @@ var queryReview = new Parse.Query(review);
 
 queryReview.equalTo("venue", venue);
 
-queryReview.find({
-                   success: function(results) {
+    queryReview.find( {
 
-var sum = 0;
+        success : function(results) {
 
- for(var i = 0; i < results.length; i++){
+        var sum = 0;
 
- sum = sum + results[i].get("rating");
+            for(var i = 0; i < results.length; i ++){
 
- }
+            sum = sum + results[i].get("rating");
 
-var rating = sum / results.length;
-
-venue.set("rating", rating);
-
-venue.save(null, {
-            success: function(venue) {
-              // Execute any logic that should take place after the object is saved.
-              alert('New object created with objectId: ' + venue.id);
-            },
-            error: function(venue, error) {
-              // Execute any logic that should take place if the save fails.
-              // error is a Parse.Error with an error code and message.
-              alert('Failed to create new object, with error code: ' + error.message);
             }
-          });
 
+        var rating = sum / results.length;
 
-                   }
-                 });
+        venue.set("rating", rating);
 
+                venue.save(null, {
+
+                    success: function(venue) {
+
+                    alert('New object created with objectId: ' + venue.id);
+
+                    },
+
+                    error: function(venue, error) {
+
+                    alert('Failed to create new object, with error code: ' + error.message);
+
+                    }
+
+                });
+
+        }
+
+    });
 
 });
